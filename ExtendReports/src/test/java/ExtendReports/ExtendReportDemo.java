811168866f24f19efb8b06d922f220ae43a3754d
Gowthamraj <yathirv4@gmail.com>
2022-02-03 11:01:23 +0530

    first commit

---
+++ ExtendReports/src/test/java/ExtendReports/ExtendReportDemo.java
@@ -0,0 +1,52 @@
+package ExtendReports;
+
+import java.time.Duration;
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import com.aventstack.extentreports.ExtentReports;
+import com.aventstack.extentreports.ExtentTest;
+import com.aventstack.extentreports.reporter.ExtentSparkReporter;
+
+
+public class ExtendReportDemo {
+	
+	ExtentReports extent;
+	
+	@BeforeTest
+	public void config()
+	{
+		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
+		reporter.config().setReportName("Web Automation results");
+		reporter.config().setDocumentTitle("test Results");
+		
+		extent = new ExtentReports();
+		extent.attachReporter(reporter);
+		extent.setSystemInfo("Tester", "Gowtham");
+	}
+	
+	@Test
+	public void initialDemo()
+	
+	{
+		ExtentTest test = extent.createTest("Demo");
+		System.setProperty("webdriver.chrome.driver", "/Users/yathi/Documents/chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		JavascriptExecutor executor = (JavascriptExecutor)driver;
+		executor.executeScript("document.body.style.zoom = '1'");
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		driver.get("https://youtube.com");
+		System.out.println(driver.getTitle());
+		driver.close();
+		test.fail("Failed");
+		extent.flush();
+		
+	}
+
+}
