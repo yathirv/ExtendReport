e9df438bcde868bf311b45867e42fbe524e3ae62
Gowthamraj <yathirv4@gmail.com>
2022-02-09 12:53:58 +0530

    feb 9

--- ExtendReports/src/test/java/ExtendReports/ExtendReportDemo.java
+++ ExtendReports/src/test/java/ExtendReports/ExtendReportDemo.java
@@ -1,8 +1,5 @@
 package ExtendReports;
 
-import java.time.Duration;
-
-import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.BeforeTest;
@@ -12,41 +9,49 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import com.aventstack.extentreports.reporter.ExtentSparkReporter;
 
-
 public class ExtendReportDemo {
-	
-	ExtentReports extent;
-	
+
+	ExtentReports extent; // Declaring ExtentReports class globally
+
 	@BeforeTest
-	public void config()
-	{
-		String path = System.getProperty("user.dir")+"\\reports\\index.html";
+	public void config() {
+		String path = System.getProperty("user.dir") + "\\reports\\index.html";
 		ExtentSparkReporter reporter = new ExtentSparkReporter(path);
 		reporter.config().setReportName("Web Automation results");
 		reporter.config().setDocumentTitle("test Results");
-		
+
 		extent = new ExtentReports();
 		extent.attachReporter(reporter);
 		extent.setSystemInfo("Tester", "Gowtham");
 	}
-	
+
 	@Test
 	public void initialDemo()
-	
+
 	{
-		ExtentTest test = extent.createTest("Demo");
+//		to configure Testcase report to the main class variable 
+
+		ExtentTest test = extent.createTest("Demo"); // to access here
+
 		System.setProperty("webdriver.chrome.driver", "/Users/chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		JavascriptExecutor executor = (JavascriptExecutor)driver;
-		executor.executeScript("document.body.style.zoom = '1'");
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		/*
+		 * driver.manage().window().maximize(); JavascriptExecutor executor =
+		 * (JavascriptExecutor)driver;
+		 * executor.executeScript("document.body.style.zoom = '1'");
+		 * driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(5));
+		 */
 		driver.get("https://youtube.com");
 		System.out.println(driver.getTitle());
+
+		// self not working
+		String path = System.getProperty("user.dir") + "\\reports\\snap.png";
+		test.addScreenCaptureFromBase64String(path);
+		
 		driver.close();
 		test.fail("Failed");
 		extent.flush();
-		
+
 	}
 
 }
